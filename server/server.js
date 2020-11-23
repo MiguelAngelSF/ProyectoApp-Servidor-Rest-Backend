4b93cbc44397939d649ad22124ee9b0bb9785336
Miguel Salas <miguelangel.sf@outlook.com>
2020-11-22 21:37:09 -0600

    Conexión lista a BD

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config')
 
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
+
 
 //parse application/x-www-fomr-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-    res.send('<h1>Bienvenido a mi servidor REST</h1>');
+    res.send('<h1>Bienvenido a mi servidor REST (localhost)</h1>');
 });
 
 app.get('/usuario', function(req, res) {
@@ -20,6 +22,7 @@ app.get('/usuario', function(req, res) {
         mensaje: 'Usuarios consultados con exito'
     });
 });
+
 
 
 app.post('/usuario', function(req, res) {
@@ -64,6 +67,15 @@ app.delete('/usuario/:id', function(req, res) {
     });
 });
 
+mongoose.connect('mongodb://localhost:27017/cafeteria', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err, res) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE')
+});
 
 app.listen(process.env.PORT, () => {
     console.log('El servidor esta en linea en el puerto', process.env.PORT)
