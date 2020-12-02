e0e7d8f6f68b5c963840e68a5ccdc8eaa1177434
Miguel Salas <miguelangel.sf@outlook.com>
2020-12-02 14:29:51 -0600

    Se creo el modelo de categoria y se creo el GET Y POST del mismo

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,5 +1,6 @@
 const { request } = require('express');
 const express = require('express');
+const { identity } = require('underscore');
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
 const app = express();
@@ -79,13 +80,41 @@ app.put('/usuario/:id', function(req, res) {
 });
 
 app.delete('/usuario/:id', function(req, res) {
+    // let id = req.params.id;
+
+    // Usuario.deleteOne({ _id: id }, (err, usuarioBorrado) => {
+    //     if (err) {
+    //         return res.status(400).json({
+    //             ok: true,
+    //             msg: 'Ocurrio un error al momento de eliminar',
+    //             err
+    //         });
+    //     }
+    //     res.json({
+    //         ok: true,
+    //         msg: 'Usuario eliminado con exito',
+    //         usuarioBorrado
+
+    //     });
+
+    // });
+
     let id = req.params.id;
 
-    res.json({
-        ok: 200,
-        mensaje: 'Usuario eliminado con exito',
-        id: id
+    Usuario.findByIdAndUpdate(id, { estado: false }, { new: true, runValidators: true, context: 'query' }, (err, usrDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: true,
+                msg: 'Ocurrio un error al momento de eliminar',
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            msg: 'Usuario eliminado con exito',
+            usrDB
 
+        });
     });
 });
 
